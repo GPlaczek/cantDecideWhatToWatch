868922015760459eeaa354f35a5de9fa51852afe
Grzegorz Płaczek <g01.placzek@gmail.com>
2022-12-13 22:10:01 +0100

    Add fire button in javax gui

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -1,5 +1,10 @@
 package com.sample;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -8,18 +13,30 @@ import org.kie.api.runtime.KieSession;
  * This is a sample class to launch a rule.
  */
 public class DroolsTest {
-
     public static final void main(String[] args) {
-        try {
-            // load up the knowledge base
-	        KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+    	JFrame f = new JFrame();
+    	JButton b = new JButton("fire");
+    	b.setBounds(130,100,100,40);
+    	b.addActionListener(new ActionListener() {
+    		@Override
+    		public void actionPerformed(ActionEvent e) {
+    			try {
+    	            // load up the knowledge base
+    		        KieServices ks = KieServices.Factory.get();
+    	    	    KieContainer kContainer = ks.getKieClasspathContainer();
+    	        	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-            // go !
-            kSession.fireAllRules();
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+    	            // go !
+    	            kSession.fireAllRules();
+    	        } catch (Throwable t) {
+    	            t.printStackTrace();
+    	        }
+    		}
+    	});
+    	f.add(b);
+    	f.setSize(400,500);
+    	f.setLayout(null);
+    	f.setVisible(true);
+        
     }
 }
